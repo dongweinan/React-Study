d069831283ec2f244e67ed3131b4a2f9806e23f1
dwn <453279413@qq.com>
2024-04-22 08:47:44 +0800

    feat: 0421

--- React 18/React-Logs/src/components/confirmModal/confirmModal.js
+++ React 18/React-Logs/src/components/confirmModal/confirmModal.js
@@ -1,19 +1,27 @@
-import './confirmModal.css'
-const ConfirmModel = (props) => {
-  return (
-    <div className="back-drop">
-      <div className="confirm-wrap">
-        <div className="confirm-content">{props.confirmText}</div>
-        <div className="confirm-btns">
-          <button className="btn ok-btn" onClick={props.okHandle}>
-            确定
-          </button>
-          <button className="btn cancel-btn" onClick={props.cancelHandle}>
-            取消
-          </button>
-        </div>
-      </div>
-    </div>
-  )
-}
-export default ConfirmModel
+import './confirmModal.css';
+import ReactDom from 'react-dom';
+
+const ConfirmModel = props => {
+    const backDropRoot = document.getElementById('back-drop');
+    return ReactDom.createPortal(
+        <div className="back-drop">
+            <div className="confirm-wrap">
+                <div className="confirm-content">{props.confirmText}</div>
+                <div className="confirm-btns">
+                    <button
+                        className="btn ok-btn"
+                        onClick={props.okHandle}>
+                        确定
+                    </button>
+                    <button
+                        className="btn cancel-btn"
+                        onClick={props.cancelHandle}>
+                        取消
+                    </button>
+                </div>
+            </div>
+        </div>,
+        backDropRoot
+    );
+};
+export default ConfirmModel;
